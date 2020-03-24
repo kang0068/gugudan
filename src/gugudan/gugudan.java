8d81f5e001db3e343b18af05643a2cb090ce9448
kangy <kangy@DESKTOP-U1Q03GS>
2020-03-24 11:52:43 +0900

    input

--- src/gugudan/gugudan.java
+++ src/gugudan/gugudan.java
@@ -1,10 +1,25 @@
 package gugudan;
 
+import java.util.Scanner;
+
 public class gugudan {
 	public static void main(String[] args){
-		for(int i=2; i<10; i++){
-			for(int j=1; j<10; j++){
-				System.out.println(i+" x "+j+" = "+i*j);
+		//all
+//		for(int i=2; i<10; i++){
+//			for(int j=1; j<10; j++){
+//				System.out.println(i+" x "+j+" = "+i*j);
+//			}
+//		}
+		
+		//input
+		System.out.println("input : ");
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		if(a<2 || a>9){
+			System.out.println("wrong number");
+		}else{
+			for(int i=1; i<10; i++){
+				System.out.println(a+" x "+i+" = "+a*i);
 			}
 		}
 	}
